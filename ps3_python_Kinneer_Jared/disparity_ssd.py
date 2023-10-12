7cdb2f757c5ed40f61e31ee58f6ecb256280b9d5
Jared Kinneer <jrkinneer@gmail.com>
2023-10-12 16:22:57 -0400

    homework3

---
+++ ps3_python_Kinneer_Jared/disparity_ssd.py
@@ -0,0 +1,57 @@
+import numpy as np
+from tqdm import *
+
+BLOCK_DIM = 7
+SEARCH_BLOCK_SIZE = BLOCK_DIM * BLOCK_DIM
+
+def disparity_ssd(L, R):
+    """Compute disparity map D(y, x) such that: L(y, x) = R(y, x + D(y, x))
+    
+    Params:
+    L: Grayscale left image
+    R: Grayscale right image, same size as L
+
+    Returns: Disparity map, same size as L, R
+    """
+    h, w = L.shape
+    disparity_map = np.zeros((h,w))
+    for y in tqdm(range(BLOCK_DIM, h - BLOCK_DIM)):
+        for x in range(BLOCK_DIM, w-BLOCK_DIM):
+            block_left = np.asarray(L[y:y+BLOCK_DIM, x:x+BLOCK_DIM])
+            
+            min_ind = compare_along_line(y, x, R, block_left, block_size=BLOCK_DIM)
+            
+            disparity_map[y, x] = abs(min_ind[1] - x)
+    
+    return disparity_map
+    # TODO: Your code here
+
+def SSD_per_block(left_block, right_block):
+    return np.sum(np.power(np.subtract(left_block, right_block), 2))
+
+def compare_along_line(row_ind, col_ind, right_img, left_block, block_size=7):
+    x_left = max(0, col_ind - SEARCH_BLOCK_SIZE)
+    x_right = min(right_img.shape[1] - block_size, col_ind + SEARCH_BLOCK_SIZE + block_size)
+    first = True
+    min_SSD = None
+    min_ind = None
+    
+    for x in range(x_left, x_right):
+        right_block = np.asarray(right_img[row_ind: row_ind+block_size, x: x+block_size])
+        
+        if (left_block.shape != right_block.shape):
+            print("\n shape: ", right_img.shape[1] - 1, " other: ", col_ind + SEARCH_BLOCK_SIZE + 1)
+            print("row_ind: ", row_ind, " col_ind: ", col_ind)
+        
+        ssd = SSD_per_block(left_block, right_block)
+        
+        if first:
+            min_SSD = ssd
+            min_ind = (row_ind,x)
+            first = False
+        else:
+            if (ssd < min_SSD):
+                min_SSD = ssd
+                min_ind = (row_ind, x)
+                
+    return min_ind
---
+++ ps3_python_Kinneer_Jared/ps2.py
@@ -0,0 +1,44 @@
+# ps2
+import os
+import numpy as np
+import cv2
+import matplotlib.pyplot as plt
+from disparity_ssd import disparity_ssd
+
+## 1-a
+# Read images
+# L = cv2.imread(os.path.join('input', 'pair0-L.png'), 0) * (1.0 / 255.0)  # grayscale, [0, 1]
+# R = cv2.imread(os.path.join('input', 'pair0-R.png'), 0) * (1.0 / 255.0)
+
+# Compute disparity (using method disparity_ssd defined in disparity_ssd.py)
+# D_L = disparity_ssd(L, R)
+# cv2.imshow("first disparity", D_L)
+# cv2.waitKey(0)
+# cv2.destroyAllWindows()
+# D_R = disparity_ssd(R, L)
+# cv2.imshow("second disparity", D_R)
+# cv2.waitKey(0)
+# cv2.destroyAllWindows()
+# cv2.imwrite("./output/ps3-1-a-1.png", D_L)
+# cv2.imwrite("./output/ps3-1-a-2.png", D_R)
+
+
+# TODO: Save output images (D_L as output/ps2-1-a-1.png and D_R as output/ps2-1-a-2.png)
+# Note: They may need to be scaled/shifted before saving to show results properly
+
+# TODO: Rest of your code here
+
+#2
+L1 = cv2.imread(os.path.join('input', 'pair1-L.png'), 0) * (1.0 / 255.0)  # grayscale, [0, 1]
+R1 = cv2.imread(os.path.join('input', 'pair1-R.png'), 0) * (1.0 / 255.0)
+
+D_L = disparity_ssd(L1, R1)
+cv2.imshow("first disparity", D_L)
+cv2.waitKey(0)
+cv2.destroyAllWindows()
+cv2.imwrite("./output/ps3-2-a-1.png", D_L)
+D_R = disparity_ssd(R1, L1)
+cv2.imshow("second disparity", D_L)
+cv2.waitKey(0)
+cv2.destroyAllWindows()
+cv2.imwrite("./output/ps3-2-a-2.png")
