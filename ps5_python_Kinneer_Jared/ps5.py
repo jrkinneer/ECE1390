e77492d733968871658af78529492d59425f8bcc
Jared Kinneer <jrkinneer@gmail.com>
2023-11-12 12:50:25 -0500

    changed code for question 1

--- ps5_python_Kinneer_Jared/ps5.py
+++ ps5_python_Kinneer_Jared/ps5.py
@@ -3,34 +3,7 @@ import cv2
 import scipy
 
 #1a
-def compute_grad_X(img, kernel_size):
-    kernel = np.zeros((1,kernel_size))
-    kernel[0, kernel_size // 2 - 1] = -1
-    kernel[0, kernel_size // 2 + 1] = 1
-    
-    grad_x = np.zeros_like(img, dtype=float)
-    
-    for i in range(img.shape[0]):
-        grad_x[i, :] = np.convolve(img[i, :], kernel.flatten(), mode='same')
-        
-    grad_x = ((grad_x - 128)/128)*127
-    return grad_x
-
-def compute_grad_y(img, kernel_size):
-    kernel = np.zeros((1,kernel_size))
-    kernel[0, kernel_size // 2 - 1] = -1
-    kernel[0, kernel_size // 2 + 1] = 1
-    
-    grad_y = np.zeros_like(img, dtype=float)
-    
-    for i in range(img.shape[1]):
-        grad_y[:, i] = np.convolve(img[:, i], kernel.flatten(), mode='same')
-        
-    grad_y = ((grad_y - 128)/128)*127
-    
-    return grad_y
-
-def new_grad(img):
+def compute_grad(img):
     kernel_x = np.array([[-1, 0, 1], [-2, 0, 2], [-1, 0, 1]])
     kernel_y = np.array([[-1, -2, -1], [0, 0, 0], [1, 2, 1]])
 
@@ -55,17 +28,14 @@ def new_grad(img):
 transA = cv2.imread("./input/transA.jpg",cv2.IMREAD_GRAYSCALE)
 simA = cv2.imread("./input/simA.jpg",cv2.IMREAD_GRAYSCALE)
 
-grad_x_transA = compute_grad_X(transA, 3)
-
-grad_y_transA = compute_grad_y(transA, 3)
+grad_x_transA , grad_y_transA = compute_grad(transA)
 
 final_trans = np.hstack((grad_x_transA, grad_y_transA))
 
-grad_x_simA = compute_grad_X(simA, 3)
-grad_y_simA = compute_grad_y(simA, 3)
+grad_x_simA , grad_y_simA = compute_grad(simA)
 final_sim = np.hstack((grad_x_simA, grad_y_simA))
-# cv2.imwrite("./output/ps5-1-a-1.png", final_trans)
-# cv2.imwrite("./output/ps5-1-a-2.png", final_sim)
+cv2.imwrite("./output/ps5-1-a-1.png", final_trans)
+cv2.imwrite("./output/ps5-1-a-2.png", final_sim)
 
 #1b
 def harris_value(grad_x, grad_y, window_size, alpha):
@@ -80,56 +50,46 @@ def harris_value(grad_x, grad_y, window_size, alpha):
     determinant = (Sx2 * Sy2) - (Sxy**2)
     trace = Sx2 + Sy2
     harris = determinant - (alpha * (trace**2))
-    #harris = ((harris - harris.min()) * (1/(harris.max() - harris.min()) * 255)).astype('uint8')
+    harris = ((harris - harris.min()) * (1/(harris.max() - harris.min()) * 255)).astype('uint8')
     return harris
 
-def non_maximal_seppression(image, window_size = 3, threshold = .01):
-    new_threshold = threshold*255
+def non_maximal_suppression(image, window_size = 3, threshold_percent = .01):
+    threshold = threshold_percent*np.max(image)
     suppressed = image.copy()
     z = window_size//2
+    count_suppressed = 0
     for i in range(z, image.shape[0] - z):
         for j in range(z, image.shape[1] - z):
             window = suppressed[i - z:i + z + 1, j - z: j + z + 1]
             
-            if suppressed[i, j] < np.max(window) or suppressed[i,j] < new_threshold:
+            
+            if suppressed[i, j] < np.max(window) or suppressed[i,j] < threshold:
                 suppressed[i,j] = 0
+                count_suppressed += 1
                 
     return suppressed
 
-harris_transA = harris_value(grad_x_transA, grad_y_transA, 3, .04)
-# cv2.imshow("original harris",harris_transA)
-# cv2.waitKey(0)
-# cv2.destroyAllWindows()
-suppressed = non_maximal_seppression(harris_transA)
-# cv2.imshow("original harris thats been suppressed",suppressed)
-# cv2.waitKey(0)
-# cv2.destroyAllWindows()
-harris_simA = harris_value(grad_x_simA, grad_y_simA, 3, .04)
+harris_transA = harris_value(grad_x_transA, grad_y_transA, 5, .04)
+harris_simA = harris_value(grad_x_simA, grad_y_simA, 5, .04)
 
 #gradients and harris values of transB and simB
 transB = cv2.imread("./input/transB.jpg",cv2.IMREAD_GRAYSCALE)
 simB = cv2.imread("./input/simB.jpg",cv2.IMREAD_GRAYSCALE)
 
-grad_x_transB = compute_grad_X(transB, 3)
-grad_y_transB = compute_grad_y(transB, 3)
+grad_x_transB , grad_y_transB = compute_grad(transB)
+grad_x_simB , grad_y_simB = compute_grad(simB)
 
-grad_x_simB = compute_grad_X(simB, 3)
-grad_y_simB = compute_grad_y(simB, 3)
-
-harris_transB = harris_value(grad_x_transB, grad_y_transB, 3, .04)
-harris_simB = harris_value(grad_x_simB, grad_y_simB, 3, .04)
-# cv2.imwrite("./output/ps5-1-b-1.png", harris_transA)
-# cv2.imwrite("./output/ps5-1-b-3.png", harris_simA)
-# cv2.imwrite("./output/ps5-1-b-2.png", harris_transB)
-# cv2.imwrite("./output/ps5-1-b-4.png", harris_simB)
+harris_transB = harris_value(grad_x_transB, grad_y_transB, 5, .04)
+harris_simB = harris_value(grad_x_simB, grad_y_simB, 5, .04)
+cv2.imwrite("./output/ps5-1-b-1.png", harris_transA)
+cv2.imwrite("./output/ps5-1-b-3.png", harris_simA)
+cv2.imwrite("./output/ps5-1-b-2.png", harris_transB)
+cv2.imwrite("./output/ps5-1-b-4.png", harris_simB)
 
 #1c
 def highlight_corners(harris_image, window_size=3, threshold_percent = .01):
-    max_ = np.max(harris_image)
-    threshold = np.max(harris_image)*threshold_percent
-    
+    threshold = np.max(harris_image) * threshold_percent
     harris = harris_image.copy()
-    harris[harris_image < threshold] = 0
     
     z = window_size//2
     
@@ -149,61 +109,29 @@ def draw_corners(corners, output_image):
     for pair in corners:
         cv2.circle(output_image, (pair[1], pair[0]), 3, (0,255,0), 2)
   
-  
-blank_image = cv2.imread("./input/transA.jpg", cv2.IMREAD_GRAYSCALE)
-color_image = cv2.imread("./input/transA.jpg")
-grad_x, grad_y = new_grad(blank_image)
-new_harris = harris_value(grad_x, grad_y, 3, .04)
-cv2.imshow('harris with new gradient', new_harris)
-cv2.waitKey(0)
-cv2.destroyAllWindows()
-cv2.imshow('suppressed harris with new gradient', non_maximal_seppression(new_harris, 5, .2))
-cv2.waitKey(0)
-cv2.destroyAllWindows()
-corners = highlight_corners(new_harris)
-suppressed_corners = highlight_corners(non_maximal_seppression(new_harris, 5, .2), .2)
-blank_copy1 = color_image.copy()
-blank_copy2 = color_image.copy()
-draw_corners(corners, blank_copy1)
-cv2.imshow('corners', blank_copy1)
-cv2.waitKey(0)
-cv2.destroyAllWindows()
-draw_corners(suppressed_corners, blank_copy2)
-cv2.imshow('corners', blank_copy2)
-cv2.waitKey(0)
-cv2.destroyAllWindows()
-
-corners_transA = highlight_corners(harris_transA, threshold_percent=.4)
+suppressed_transA = non_maximal_suppression(harris_transA, 5, .1)
+corners_transA = highlight_corners(suppressed_transA, 5, .3)
 highlighted_transA = cv2.imread("./input/transA.jpg")
 draw_corners(corners_transA, highlighted_transA)
-cv2.imshow("transA with corners, original", highlighted_transA)
-cv2.waitKey(0)
-cv2.destroyAllWindows()
+cv2.imwrite("./output/ps5-1-c-1.png", highlighted_transA)
 
-corners_suppressed = highlight_corners(suppressed, threshold_percent=.4)
-s = cv2.imread("./input/transA.jpg")
-draw_corners(corners_suppressed, s)
-cv2.imshow("transA with corners, surpressed", s)
-cv2.waitKey(0)
-cv2.destroyAllWindows()
-
-
-# cv2.imwrite("./output/ps5-1-c-1.png", highlighted_transA)
-
-corners_transB = highlight_corners(harris_transB, threshold_percent=.4)
+suppressed_transB = non_maximal_suppression(harris_transB, 5, .1)
+corners_transB = highlight_corners(suppressed_transB, 5, .3)
 highlighted_transB = cv2.imread("./input/transB.jpg")
 draw_corners(corners_transB, highlighted_transB)
-# cv2.imwrite("./output/ps5-1-c-2.png", highlighted_transB)
+cv2.imwrite("./output/ps5-1-c-2.png", highlighted_transB)
 
-corners_simA = highlight_corners(harris_simA, threshold_percent=.4)
+suppressed_simA = non_maximal_suppression(harris_simA, 5, .1)
+corners_simA = highlight_corners(suppressed_simA, 5, .3)
 highlighted_simA = cv2.imread("./input/simA.jpg")
 draw_corners(corners_simA, highlighted_simA)
-# cv2.imwrite("./output/ps5-1-c-3.png", highlighted_simA)
+cv2.imwrite("./output/ps5-1-c-3.png", highlighted_simA)
 
-corners_simB = highlight_corners(harris_simB, threshold_percent=.55)
+suppressed_simB = non_maximal_suppression(harris_simB, 5, .1)
+corners_simB = highlight_corners(suppressed_simB, 5, .3)
 highlighted_simB = cv2.imread("./input/simB.jpg")
 draw_corners(corners_simB, highlighted_simB)
-# cv2.imwrite("./output/ps5-1-c-4.png", highlighted_simB)
+cv2.imwrite("./output/ps5-1-c-4.png", highlighted_simB)
 
 #2a
 def compute_angle(grad_x, grad_y):
