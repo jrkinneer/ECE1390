0b904992d17d2deaa3d9127b2862805161542f18
Jared Kinneer <jrkinneer@gmail.com>
2023-12-05 14:00:25 -0500

    finished question 1, needs testing

--- ps7_python_Kinneer_Jared/ps7.py
+++ ps7_python_Kinneer_Jared/ps7.py
@@ -20,7 +20,7 @@ main_path = "./input/p1/train_imgs"
 X_train = np.zeros((1, car_hog.shape[0]))
 y_train = np.zeros((1,1))
 
-for file in tqdm(os.listdir(main_path), ):
+for file in tqdm(os.listdir(main_path), "files in input directory"):
     img = cv2.imread(main_path + "/" + file)
     hog_vector = hog(img, cells_per_block = (2,2,), channel_axis = 2)
     np.vstack((X_train, hog_vector))
@@ -41,15 +41,38 @@ svm_classifier.fit(X_train, y_train)
 test_path = "./input/p1/test_imgs"
 
 window_size = (96, 32)
-for file in os.listdir(test_path):
+K = 0
+#for file in test directory
+for file in tqdm(os.listdir(test_path), "file in test directory"):
     img = np.asarray(cv2.imread(test_path + "/" + file))
+    
+    i_j_list = []
+    car_prob = []
+    
+    #for all possible window locations
     for i in range(img.shape[0] - window_size[0]):
         for j in range(img.shape[1] - window_size[1]):
+            #get window
             window = img[i:window_size[0], j:window_size[1]]
             
+            #window hog
             window_hog = hog(window, cells_per_block = (2,2,), channel_axis = 2)
             
+            #predict car or not car
             results_vector = svm_classifier.decision_function(window_hog)
-    hog_vector = hog(img, cells_per_block = (2,2,), channel_axis = 2)
+            
+            #record data
+            i_j_list.append((i,j))
+            car_prob.append(results_vector[1])
+            
+    #get highest prob in car_prob
+    ind = np.argmax(car_prob)
+    val = np.max(car_prob)
     
-    
+    #draw rectangle if above threshold and save image regardless
+    if val > 1.2:
+        rectangle_img = cv2.rectangle(img, i_j_list[ind][0], i_j_list[ind][1])
+        cv2.imwrite("./output/ps7-1-d-"+K+".png", rectangle_img)
+    else:
+        cv2.imwrite("./output/ps7-1-d-"+K+".png", img)
+            
