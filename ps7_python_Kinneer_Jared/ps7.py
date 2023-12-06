320f9a8568a72f408450c4d2c1d1bb03596a1c50
Jared Kinneer <jrkinneer@gmail.com>
2023-12-06 17:16:38 -0500

    report and question 2

--- ps7_python_Kinneer_Jared/ps7.py
+++ ps7_python_Kinneer_Jared/ps7.py
@@ -8,6 +8,7 @@ import os
 #1a
 car_1 = cv2.imread("./input/p1/car.jpg")
 car_hog, car_hog_img = hog(car_1, cells_per_block = (2,2,), channel_axis = 2, visualize = True)
+print(car_hog.shape)
 # cv2.imshow("hog", car_hog_img)
 # cv2.waitKey(0)
 # cv2.destroyAllWindows()
@@ -79,4 +80,89 @@ for file in tqdm(os.listdir(test_path), "file in test directory", leave=False):
         cv2.imwrite("./output/ps7-1-d-"+str(K)+".png", img)
         
     K+=1
-            
+  
+#2       
+
+import matplotlib.pyplot as plt
+from matplotlib import gridspec
+import numpy as np
+import tensorflow as tf
+# from tensorflow.keras.preprocessing import image_dataset_from_directory
+# from tensorflow import keras
+# from tensorflow.keras import layers
+# Load training and validation sets
+ds_train_ = tf.keras.preprocessing.image_dataset_from_directory(
+    './input/p2/train_imgs',
+    labels='inferred',
+    label_mode='categorical',
+    image_size=[32, 32],
+    batch_size=100,
+    shuffle=True,
+)
+ds_test_ = tf.keras.preprocessing.image_dataset_from_directory(
+    './input/p2/test_imgs',
+    labels='inferred',
+    label_mode='categorical',
+    image_size=[32, 32],
+    batch_size=100,
+    shuffle=False,
+)
+# Define the model
+model = tf.keras.Sequential([
+    # First Convolutional Block
+    tf.keras.layers.Conv2D(filters=32, kernel_size=5, activation="relu", padding='same',
+    # give the input dimensions in the first layer
+    # [height, width, color channels(RGB)]
+    input_shape=[32, 32, 3]),
+    tf.keras.layers.BatchNormalization(),
+    tf.keras.layers.MaxPool2D(),
+    # Second Convolutional Block
+    tf.keras.layers.Conv2D(filters=32, kernel_size=5, activation="relu", padding='same'),
+    tf.keras.layers.BatchNormalization(),
+    tf.keras.layers.MaxPool2D(),
+    # third Convolutional Block
+    tf.keras.layers.Conv2D(filters=64, kernel_size=5, activation="relu", padding='same'),
+    tf.keras.layers.BatchNormalization(),
+    tf.keras.layers.MaxPool2D(),
+    # Classifier Head
+    tf.keras.layers.Flatten(),
+    tf.keras.layers.Dense(units=27, activation="relu"),
+    tf.keras.layers.Dense(units=3, activation="softmax"),
+    
+])
+model.summary()
+#train the model
+model.compile(
+    optimizer='adam',
+    loss='categorical_crossentropy',
+    metrics=['accuracy']
+)
+history = model.fit(
+    ds_train_,
+    #validation_data=ds_valid_,
+    epochs=10,
+)
+plt.plot(history.history['accuracy'], label='accuracy')
+#plt.plot(history.history['val_accuracy'], label = 'val_accuracy') # validation accuracy; no validation in this example
+plt.xlabel('Epoch')
+plt.ylabel('Accuracy')
+plt.ylim([0, 1])
+plt.legend(loc='lower right')
+plt.show()
+#test the model
+scores = model.evaluate(ds_test_, verbose=0)
+print('Accuracy on testing data: {}% \n Error on training data: {} \n'.format(scores[1], 1 - scores[1]))
+print(model.predict(ds_test_))
+# #displaying an image with the detected label
+# from keras.preprocessing import image
+# im = image.load_img('./input/p2/test_imgs/Blue/2_00.jpg')
+# im = np.expand_dims(im, axis=0)
+# pred = model.predict(im, verbose=0)
+# print(pred)
+# classes = ["Black", "Blue", "Green", "No car"]
+# class_ID = np.argmax(pred)
+# title = 'predicted ' + classes[class_ID]
+# plt.imshow(tf.squeeze(im))
+# plt.axis('off')
+# plt.title(title)
+# plt.show()
